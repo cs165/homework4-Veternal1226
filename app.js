ba7c62d8d0cca9504d0981852955f95308728f7a
Veternal1226 <lwj851226@yahoo.com.tw>
2019-05-22 10:49:11 +0800

    Update app.js

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ class App {
 
   // TODO(you): Add methods as necessary.
   ToMusic(event){
-    console.log('1');
+    console.log(event.detail);
     this.menuContainer.classList.add("inactive");
     this.musicContainer.classList.remove("inactive");
   }
