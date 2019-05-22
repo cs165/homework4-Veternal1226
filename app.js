8e71b3c03945a95379bbaca12c73898c4504bb89
Veternal1226 <lwj851226@yahoo.com.tw>
2019-05-22 13:37:36 +0800

    veter

--- app.js
+++ app.js
@@ -24,6 +24,27 @@ class App {
     console.log(event.detail);
     this.menuContainer.classList.add("inactive");
     this.musicContainer.classList.remove("inactive");
+
+    const URL = this.URL + encodeURIComponent(event.detail.gifValue) + "&api_key=FjJaTP04iY5rAwcEASKET51wyx9VZ2V8&limit=25&rating=g";
+    const onJsonReady = (json) => {
+      let imgURL = [];
+      if(json.data.length > 2) {
+        for(let index in json.data) {
+          const imgurl = json.data[index].images.downsized.url;           
+          imgURL.push(imgurl);
+        }
+        //this.menuScreen.hideErrMsg();
+        //this.menuScreen.hide();
+        //this.musicScreen.preload(imgURL, event.detail.songValue);
+      }else {
+        //this.menuScreen.showErrMsg();
+      }
+    };
+
+    fetch(URL)
+      .then(response => response.json())
+      .then(onJsonReady);
+
   }
 
   ToMenu(event){
